46b4a23dcf39a088b3deb7ceb6254325ee85917b
Bensa0 <basel.a2001@gmail.com>
2022-09-27 02:15:29 +0200

    counterDone

--- app/src/main/java/com/example/prayerstoallah/NightPrayersActivity.kt
+++ app/src/main/java/com/example/prayerstoallah/NightPrayersActivity.kt
@@ -2,26 +2,31 @@ package com.example.prayerstoallah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import org.w3c.dom.Text
 
 class NightPrayersActivity : AppCompatActivity() {
-    private lateinit var button: ImageView
-    private lateinit var button2: ImageView
+    private lateinit var llKorsi: LinearLayout
+    private lateinit var tvKorsiCounter: TextView
+    private lateinit var llKorsiCounter: LinearLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_night_prayers)
 
-        button = findViewById(R.id.ivButton)
-        button2 = findViewById(R.id.ivImageView)
+        llKorsi = findViewById(R.id.llAyetElKorsi)
+        tvKorsiCounter = findViewById(R.id.tvCounter1)
+        llKorsiCounter = findViewById(R.id.llCounter1)
+        llKorsi.setOnClickListener {
+            var counter: Int = tvKorsiCounter.text.toString().toInt()
+            counter--
+            tvKorsiCounter.text = counter.toString()
+            if (counter == 0) {
+                llKorsiCounter.visibility=View.GONE
+            }
 
-        button.setOnClickListener {
-            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-        }
-
-        button2.setOnClickListener {
-            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
         }
     }
 }
